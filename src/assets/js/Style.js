83a774faa80f7af3b86e4c4d97b8f7da1feb2cd9
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-05 18:19:10 +0530

    update

--- src/assets/js/Style.js
+++ src/assets/js/Style.js
@@ -1,22 +1,37 @@
-let  move_speed =3, grativy =0.5;
+let move_speed = 3, grativy = 0.5;
 let bird = document.querySelector('.bird');
 let img = document.getElementById('bird-1');
 
-let bird_props =bird.getBoundingClientRect();
+let bird_props = bird.getBoundingClientRect();
 
 let background = document.querySelector('.background').getBoundingClientRect();
 let score_val = document.querySelector('.score_val');
 let message = document.querySelector('.message');
-let score_title =document.querySelector('.score_title');
+let score_title = document.querySelector('.score_title');
 
 let game_state = 'Start';
-img.style.display ='none';
+img.style.display = 'none';
 message.classList.add('messageStyle');
 
-document.addEventListener('keydown',(e) =>{
-    if (e.key=='Enter' && game_state != 'play'){
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter' && game_state != 'Play') {
         document.querySelectorAll('.pipe_sprite').forEach((e) => {
             e.remove()
-        })
+        });
+        img.style.display = 'block';
+        bird.style.top = '40vh';
+        game_state = 'Play';
+        message.innerHTML = '';
+        score_title.innerHTML = 'Score : ';
+        score_val.innerHTML = '0';
+        message.classList.remove('messageStyle');
+        play();
+
     }
-})
+});
+
+function play() {
+    function move() {
+
+    }
+}
