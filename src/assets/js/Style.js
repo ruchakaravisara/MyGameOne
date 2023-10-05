9fc3960d9aceb803eae7cf30e14de8fb8628354e
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-05 17:08:39 +0530

    update

---
+++ src/assets/js/Style.js
@@ -0,0 +1,7 @@
+let  move_speed =3, grativy =0.5;
+let bird = document.querySelector('.bird');
+let img = document.getElementById('bird-1');
+
+let bird_props =bird.getBoundingClientRect();
+
+let background = document.querySelector('.background').getBoundingClientRect();
