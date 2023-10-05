951dd0026c3ef889dba0362207aa96d1cd5a8221
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-05 21:31:20 +0530

    updated

--- src/assets/js/Style.js
+++ src/assets/js/Style.js
@@ -39,9 +39,31 @@ function play() {
             let pipe_sprite_props = element.getBoundingClientRect();
             bird_props = bird.getBoundingClientRect();
 
+            if (pipe_sprite_props.right <= e) {
+                element.remove();
+            } else {
+                if (bird_props.left < pipe_sprite_props.left + pipe_sprite_props.width && bird_props.left + bird_props.width > pipe_sprite_props.left && bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height && bird_props.top + bird_props.height > pipe_sprite_props.top){
+                    game_state = 'End';
+                    message.innerHTML = 'Game Over'.fontcolor('red') + '<br> Press Enter To Restart';
+                    message.classList.add('messageStyle');
+                    img.style.display = 'none';
+                    return;
+                } else {
+                    if (pipe_sprite_props.right < bird_props.left && pipe_sprite_props.right + move_speed >= bird_props.left && element.increse_score == '1') {
+                        score_val.innerHTML = +score_val.innerHTML + 1;
+                    }
+                    element.style.left = pipe_sprite_props.left - move_speed + 'px';
+                }
 
+
+            }
         });
-
+                requestAnimationFrame(move);
     }
-
+    requestAnimationFrame(move);
+    let bird_dy =0;
+    function apply_gravity() {
+        if (game_state != 'Play') return;
+        
+    }
 }
